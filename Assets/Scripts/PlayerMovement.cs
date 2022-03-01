7b2137f9a48cb5c0cbf27c05b5bd3f3c1776b9a1
LuisLiraC <luis_ssbm@hotmail.com>
2022-02-28 22:04:52 -0600

    3.3. Programación de mecánicas y físicas

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,23 +7,46 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private GameObject Bullet;
+    [SerializeField]
+    private float JumpForce;
 
     private Animator Animator;
     private float Horizontal;
+    private Rigidbody2D Rigidbody;
+    private bool IsTouchingTheGround;
+
 
     void Start()
     {
         Animator = GetComponent<Animator>();
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody.velocity = new Vector2(Horizontal, Rigidbody.velocity.y);
     }
 
     void Update()
     {
+        Horizontal = Input.GetAxis("Horizontal");
+        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
+
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+            IsTouchingTheGround = true;
+        else
+            IsTouchingTheGround = false;
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Shoot();
         }
 
-        Horizontal = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsTouchingTheGround)
+        {
+            Jump();
+        }
+        
 
         if (Horizontal < 0)
         {
@@ -43,5 +66,10 @@ public class PlayerMovement : MonoBehaviour
         Vector3 direction = transform.localScale.x == 1f ? Vector2.right : Vector2.left;
         var bullet = Instantiate(Bullet, transform.position + direction * 0.1f, Quaternion.identity);
         bullet.GetComponent<Bullet>().SetDirection(direction);
+    }
+
+    void Jump()
+    {
+        Rigidbody.AddForce(Vector2.up * JumpForce);
     }
 }
